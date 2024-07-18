28e02d1b249c640ba950306a7468d3e66fecc3cf
KhushbooPandey08 <149251051+KhushbooPandey08@users.noreply.github.com>
2024-07-19 00:26:43 +0530

    Time: 35 ms (73.41%), Space: 35.6 MB (5.63%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        std::unordered_map<int, int> map;
-        map[0] = 1;
-        int prefix_sum = 0;
-        int count = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            prefix_sum += nums[i];
-            int remainder = prefix_sum % k;
-            if (remainder < 0) {
-                remainder += k;
+       unordered_map<int, int>mp;
+        mp[0]=1;
+        int count=0;
+        int sum=0;
+        for(int i=0;i<nums.size(); i++)
+        {
+            sum+=nums[i];
+            int rem = sum%k;
+            if(rem<0)
+            {
+                rem+=k;
             }
-            if (map.find(remainder) != map.end()) {
-                count += map[remainder];
-                map[remainder]++;
-            } else {
-                map[remainder] = 1;
+            if(mp.find(rem)!=mp.end())
+            {
+                count+= mp[rem];
+                mp[rem]++;
+            }
+            else
+            {
+                mp[rem] =1;
             }
         }
         return count;
