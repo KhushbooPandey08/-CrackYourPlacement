9fcaa932e80e3f1c933f36dcc4b57d70cca80f96
KhushbooPandey08 <149251051+KhushbooPandey08@users.noreply.github.com>
2024-07-25 15:48:54 +0530

    Time: 301 ms (72.04%), Space: 10 MB (74.18%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-         int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
+  int m, n;
+    vector<vector<int>> directions{{1,0}, {-1,0}, {0,1}, {0,-1}};
+    bool find(vector<vector<char>>& board,int i, int j, int idx, string& word)
+    {
+        if(idx==word.length())
+        {
+            return true;
+        }
+
+        if(i<0||j<0|| i>=m|| j>=n|| board[i][j]=='$' || board[i][j]!= word[idx])
+            {
                 return false;
             }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
-            return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
+
+         char temp = board[i][j];
+         board[i][j]= '$';
+        for(auto &dir : directions)
+        {
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
+            if(find(board, new_i, new_j, idx+1, word))
+                {
                     return true;
                 }
+        }
+        board[i][j]=temp;
+
+        return false;       
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size();
+        n = board[0].size(); 
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==word[0] && find( board, i, j, 0, word))
+                {
+                    return true;
+                }
+                
             }
         }
         return false;
